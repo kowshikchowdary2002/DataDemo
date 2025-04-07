9f860b31f60bfc803462aab32e06e841a3d795bf
kowshikchowdary2002 <saikowshikmullaputi@gmail.com>
2025-04-07 16:17:28 +0530

    Add files via upload

---
+++ arrayDemo.py
@@ -0,0 +1,14 @@
+import array
+# a=array.array('u',"kowshikchowdary","kowshik")# type error it take only 2 arguments 
+# #print(a[0])
+
+# for x in a:
+#             print(x)
+
+# b=array.array('i',[1,2,3])
+# length=len(b)
+# print(length)
+
+c=array.array('i',[1,2,3,5,6,8,4,3])
+sort=sorted(c)
+print(sort)
